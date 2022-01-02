e5b3b6ea98c3aad4af8f1fad2a77373fa9e038b5
BEYZANUR AKKUZU <beyzaakkuzu@gmail.com>
2022-01-02 17:09:47 +0300

    init

--- app/src/main/java/com/beyzaakkuzu/myservice/MainActivity.kt
+++ app/src/main/java/com/beyzaakkuzu/myservice/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.beyzaakkuzu.myservice
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var mIsBound: Boolean = false
+    var mService: MyService? = null
+
+    lateinit var progressBar: ProgressBar
+
     var button: Button? = null
     var button2: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,14 +26,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button = findViewById(R.id.buttonStart)
         button2 = findViewById(R.id.buttonStop)
 
+        progressBar = findViewById(R.id.progressBar)
         button!!.setOnClickListener(this)
         button2!!.setOnClickListener(this)
+
+       if(MyService.isServiceRunning) {
+           bindService(Intent(this,MyService::class.java),serviceConnection, Context.BIND_AUTO_CREATE )
+
+       }
     }
 
     override fun onClick(v: View?) {
         when (v!!.id) {
             R.id.buttonStart -> {
                 startService(Intent(this,MyService::class.java))
+                bindService(Intent(this,MyService::class.java),serviceConnection, Context.BIND_AUTO_CREATE )
             }
             R.id.buttonStop ->{
                 stopService(Intent(this, MyService::class.java))
@@ -30,4 +48,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         }
     }
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, iBinder: IBinder) {
+            val binder = iBinder as MyService.MyBinder
+            mService = binder.service
+            mIsBound = true
+            observer()
+
+
+        }
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mIsBound = false
+            Log.println(Log.ASSERT, "ONSERVICE DISCONNECTED", "it")
+        }
+    }
+    fun observer(){
+        mService!!.Status.observe(this,{
+            progressBar.setProgress(it)
+
+        })
+    }
+
 }
--- app/src/main/java/com/beyzaakkuzu/myservice/MyService.kt
+++ app/src/main/java/com/beyzaakkuzu/myservice/MyService.kt
@@ -1,30 +1,104 @@
 package com.beyzaakkuzu.myservice
 
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.*
 
 
 class MyService: Service() {
+    var number: Int? = 0
+    private var mBinder: IBinder = MyBinder()
+    var Status = MutableLiveData<Int>()
 
+    companion object{
+        var isServiceRunning:Boolean= false
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate() {
-
-
+        show_Notification()
+        isServiceRunning= true
     }
 
+
     override fun onStart(intent: Intent?, startid: Int) {
+        getShow()
+
         Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show()
+        GlobalScope.launch {
+            withContext(Dispatchers.Main){
+                for(x in 0..100){
+                    number=x
+                    job()
+                }
+            }
+
+        }
+
 
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
+        return mBinder
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show()
     }
+    inner class MyBinder : Binder() {
+        // Return this instance of MyService so clients can call public methods
+        val service: MyService
+            get() =// Return this instance of MyService so clients can call public methods
+                this@MyService
+    }
+
+    suspend fun job(){
+        delay(350)
+        Status.postValue(number)
+
+    }
+    private fun getShow(): LiveData<Int> {
+        if (Status == null) {
+            Status = MutableLiveData()
+            loadShow()
+        }
+        return Status as MutableLiveData<Int>
+    }
+    private fun loadShow() {
+        Status!!.value = 0
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun show_Notification() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        val CHANNEL_ID = "MYCHANNEL"
+        val notificationChannel =
+            NotificationChannel(CHANNEL_ID, "name", NotificationManager.IMPORTANCE_LOW)
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 1, intent, 0)
+        val notification: Notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setContentText("Heading")
+            .setContentTitle("subheading")
+            .setContentIntent(pendingIntent)
+            .setChannelId(CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+        startForeground(1,notification)
+        notificationManager.notify(1, notification)
+
+    }
+
+
+
 
 }
